201bd6ceab1bf68c83483f6068ff9ef439da411e
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2023-09-15 00:16:19 -0400

    build static

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default function Portfolio() {
     <main className={styles.main}>
         <div className={styles.center}>
             <div className={styles.welcome}> 
-                <h1>Hi there, I'm Cole</h1>
+                <h1>Hi there, I&pos;m Cole</h1>
                 <p>
                 A passionate developer who loves to make blazingly fast software and everything lower level.
                 I mainly program in Rust but I also have experience with Java, Python and JavaScript/TypeScript. 
@@ -88,7 +88,7 @@ export default function Portfolio() {
                     keyboard-rs is an open-source keyboard firmware written in Rust. 
                     It allows users to not have to worry about cryptic compiler errors and just focus on their keymap.
                     Currently keyboard-rs only works with a keyboard running off of a rp2040 
-                    but with plans on moving from <a href="https://crates.io/crates/rp2040-hal">rp2040-hal</a> to
+                    but with plans on moving from <a href="https://crates.io/crates/rp2040-hal">rp2040-hal</a> to 
                     <a href="https://crates.io/crates/embedded-hal">embedded-hal</a>, this would allow for any chip that has an embedded-hal implementation
                     including the rp2040. The only other additional feature that is implemented is rotary encoder support.
                     <br/>
@@ -111,8 +111,8 @@ export default function Portfolio() {
                     <b>WARNING VERY ACTIVE DEVLOPEMNT, INFORMATION MAY BE OUT DATED.</b>
                     <p>
                     streaming-rs is a full streaming service written in Rust. There 2 main parts, the server and the client.
-                    Let's start with the server, the server as of September 14th, 2023 is very simple, the client sends a song Spotify ID, and the server sends the file back.
-                    If the server doesn't have the file locally it downloads the song using <a href="https://github.com/spotDL/spotify-downloader">spotDL</a> and sends the file once it is done. Now the client,
+                    Let&pos;s start with the server, the server as of September 14th, 2023 is very simple, the client sends a song Spotify ID, and the server sends the file back.
+                    If the server doesn&pos;t have the file locally it downloads the song using <a href="https://github.com/spotDL/spotify-downloader">spotDL</a> and sends the file once it is done. Now the client,
                     there is a library provided by streaming-rs which is used to make clients for the server. There are currently 2 clients, a command line and a TUI, 
                     with a planned GUI. The client library currently has the following features, a library, downloading songs, searching songs, playlist and built-in shuffle.
                     </p>
@@ -136,71 +136,71 @@ export default function Portfolio() {
 
 
 
-export function Home1() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-      </div>
-
-      <div className={styles.center}>
-        <p>
-        My portfolio made with <a href="https://bun.sh/">bun</a> and <a href="https://nextjs.org/">Next.js</a>
-        <br></br>
-        By Cole Kauder-McMurrich
-        </p>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            keyboard-rs <span>-&gt;</span>
-          </h2>
-          <p>Keyboard firmware written in rust.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+// export function Home1() {
+//   return (
+//     <main className={styles.main}>
+//       <div className={styles.description}>
+//       </div>
+//
+//       <div className={styles.center}>
+//         <p>
+//         My portfolio made with <a href="https://bun.sh/">bun</a> and <a href="https://nextjs.org/">Next.js</a>
+//         <br></br>
+//         By Cole Kauder-McMurrich
+//         </p>
+//       </div>
+//
+//       <div className={styles.grid}>
+//         <a
+//           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className={styles.card}
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2>
+//             keyboard-rs <span>-&gt;</span>
+//           </h2>
+//           <p>Keyboard firmware written in rust.</p>
+//         </a>
+//
+//         <a
+//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className={styles.card}
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2>
+//             Learn <span>-&gt;</span>
+//           </h2>
+//           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+//         </a>
+//
+//         <a
+//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className={styles.card}
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2>
+//             Templates <span>-&gt;</span>
+//           </h2>
+//           <p>Explore the Next.js 13 playground.</p>
+//         </a>
+//
+//         <a
+//           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className={styles.card}
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2>
+//             Deploy <span>-&gt;</span>
+//           </h2>
+//           <p>
+//             Instantly deploy your Next.js site to a shareable URL with Vercel.
+//           </p>
+//         </a>
+//       </div>
+//     </main>
+//   )
+// }
